e12b8c13a5015a5a925560649770d67fcb7410e4
Redstone233 <3356168312@qq.com>
2025-10-19 17:14:53 +0800

    Initial commit

--- src/main/java/net/redstone233/redextent/core/mod/SuperFurnaceRegistration.java
+++ src/main/java/net/redstone233/redextent/core/mod/SuperFurnaceRegistration.java
@@ -12,21 +12,21 @@ public class SuperFurnaceRegistration {
     public static void registerAll(PonderSceneRegistrationHelper<ResourceLocation> helper) {
         // 注册超级熔炉场景
         helper.addStoryBoard(
-                ResourceLocation.withDefaultNamespace("super_furnace"),
+                ResourceLocation.fromNamespaceAndPath(RedExtendMod.MOD_ID,"super_furnace"),
                 "super_furnace_structure",
                 SuperFurnaceScene::superFurnace
         ).highlightAllTags();
 
         // 注册超级高炉场景
         helper.addStoryBoard(
-                ResourceLocation.withDefaultNamespace("super_blast_furnace"),
+                ResourceLocation.fromNamespaceAndPath(RedExtendMod.MOD_ID,"super_blast_furnace"),
                 "super_blast_furnace_structure",
                 SuperBlastFurnaceScene::superBlastFurnace
         ).highlightAllTags();
 
         // 注册超级烟熏炉场景
         helper.addStoryBoard(
-                ResourceLocation.withDefaultNamespace("super_smoker"),
+                ResourceLocation.fromNamespaceAndPath(RedExtendMod.MOD_ID,"super_smoker"),
                 "super_smoker_structure",
                 SuperSmokerScene::superSmoker
         ).highlightAllTags();
