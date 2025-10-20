647ca350f34737e662f22d2cc994867e1cceabcc
Redstone233 <3356168312@qq.com>
2025-10-20 08:17:56 +0800

    Initial commit

--- src/main/java/net/redstone233/redextent/core/mod/SuperFurnaceRegistration.java
+++ src/main/java/net/redstone233/redextent/core/mod/SuperFurnaceRegistration.java
@@ -13,21 +13,21 @@ public class SuperFurnaceRegistration {
         // 注册超级熔炉场景
         helper.addStoryBoard(
                 ResourceLocation.withDefaultNamespace("furnace"),
-                "super_furnace_structure",
+                "super_furnace/super_furnace_structure",
                 SuperFurnaceScene::superFurnace
         ).highlightAllTags();
 
         // 注册超级高炉场景
         helper.addStoryBoard(
                 ResourceLocation.withDefaultNamespace("blast_furnace"),
-                "super_blast_furnace_structure",
+                "super_blast_furnace/super_blast_furnace_structure",
                 SuperBlastFurnaceScene::superBlastFurnace
         ).highlightAllTags();
 
         // 注册超级烟熏炉场景
         helper.addStoryBoard(
                 ResourceLocation.withDefaultNamespace("smoker"),
-                "super_smoker_structure",
+                "super_smoker/super_smoker_structure",
                 SuperSmokerScene::superSmoker
         ).highlightAllTags();
     }
